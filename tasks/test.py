953deef5fe5f57f1c29de5af645f48caa64709f9
arulrajgopal <arulrajgopal@oulook.com>
2026-07-14 15:26:46 +0530

    update

--- tasks/test.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-# Get current date and time
-now = datetime.datetime.now()
-
-# Print message with timestamp
-print("Hello, World! Current UTC time is:", now.strftime("%Y-%m-%d %H:%M:%S"))
